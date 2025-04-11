7bc0cb5510a9d670b905db333a56624e1f92b04e
marcello de freitas moreira <marcellofreitas1227@gmail.com>
2025-04-10 22:09:11 -0300

    adicionado hover e ajustado JS

--- script.js
+++ script.js
@@ -59,34 +59,57 @@ document.querySelectorAll('.carrossel-container').forEach((carrosselContainer) =
 
 let lastClickedDepartamento = null;
 
+
+
+
 function clickMenu1() {
     const TodasCatItens = document.getElementById('TodasCatItens');
     const DepartamentoMenu = document.getElementById('DepartamentoMenu');
+    const iBurguer = document.getElementById('iBurguer');
 
+    // Fecha o menu de departamentos e limpa seleção de departamentos
     DepartamentoMenu.style.display = 'none';
+    lastClickedDepartamento = null;
 
+    // Remove classe 'ativo' de todos os departamentos
+    document.querySelectorAll('.dep-link').forEach(link => {
+        link.classList.remove('ativo');
+    });
+
+    // Alterna o menu de categorias e a cor do botão
     if (TodasCatItens.style.display === 'none' || TodasCatItens.style.display === '') {
         TodasCatItens.style.display = 'flex';
+        iBurguer.classList.add('ativo');
     } else {
         TodasCatItens.style.display = 'none';
+        iBurguer.classList.remove('ativo');
     }
-
-    lastClickedDepartamento = null; 
 }
+
 
 function clickMenu2(element) {
     const DepartamentoMenu = document.getElementById('DepartamentoMenu');
     const TodasCatItens = document.getElementById('TodasCatItens');
-
-    TodasCatItens.style.display = 'none';
-
     const departamentoID = element.getAttribute('data-departamento');
+    const currentLink = element.querySelector('.dep-link');
+    const iBurguer = document.getElementById('iBurguer');
 
+    // Esconde o menu de categorias e tira a cor azul do botão iBurguer
+    TodasCatItens.style.display = 'none';
+    iBurguer.classList.remove('ativo');
+
+    // Remove a classe 'ativo' de todos os departamentos
+    document.querySelectorAll('.dep-link').forEach(link => {
+        link.classList.remove('ativo');
+    });
+
+    // Alterna o menu de departamentos e a seleção
     if (lastClickedDepartamento === departamentoID && DepartamentoMenu.style.display === 'flex') {
         DepartamentoMenu.style.display = 'none';
         lastClickedDepartamento = null;
     } else {
         DepartamentoMenu.style.display = 'flex';
+        currentLink.classList.add('ativo');
         lastClickedDepartamento = departamentoID;
     }
 }
